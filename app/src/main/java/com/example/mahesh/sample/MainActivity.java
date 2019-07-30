c13656015b4440bfea68218d2752a1e19fe8bde9
Deepika333 <deepika333@github.com>
2019-07-30 21:27:04 +0530

    first commit

---
+++ app/src/main/java/com/example/mahesh/sample/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.mahesh.sample;
+
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.gubbala.ds.wannderapp.R;
+
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener, StepListener{
+
+
+    private StepDetector simpleStepDetector;
+    private SensorManager sensorManager;
+    private Sensor accel;
+    private int numSteps;
+    private TextView TvSteps;
+    private Button BtnStop , BtnStart;
+    private ProgressBar progressBar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get an instance of the SensorManager
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        simpleStepDetector = new StepDetector();
+        simpleStepDetector.registerListener(this);
+        //Progress Bar
+        progressBar = findViewById(R.id.circularProgressbar);
+
+        TvSteps =  findViewById(R.id.tv_steps);
+        BtnStart =  findViewById(R.id.btn_start);
+        BtnStop =  findViewById(R.id.btn_stop);
+
+        BtnStart.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                numSteps = 0;
+                sensorManager.registerListener(MainActivity.this, accel, SensorManager.SENSOR_DELAY_FASTEST);
+
+            }
+        });
+
+        BtnStop.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                sensorManager.unregisterListener(MainActivity.this);
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            simpleStepDetector.updateAccel(
+                    event.timestamp, event.values[0], event.values[1], event.values[2]);
+        }else {
+            Toast.makeText(this, "Sensor Not Found", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void step(long timeNs) {
+        numSteps++;
+        progressBar.setProgress(numSteps);
+        TvSteps.setText(numSteps+"");
+    }
+}
---
+++ app/src/main/java/com/gubbala/ds/wannderapp/StepListener.java
@@ -0,0 +1,10 @@
+package com.gubbala.ds.wannderapp;
+
+/**
+ * Created by Gubbala on 13-03-2018.
+ */
+
+public interface StepListener {
+
+    public void step(long timeNs);
+}
